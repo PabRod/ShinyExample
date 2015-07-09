e7daea9db2d9f2514d220111d8b262bb8802f371
Pablo Rodríguez <pablo.rodriguez.sanchez@gmail.com>
2015-07-09 17:41:12 +0200

    Version 1.1
    Just changes in the ui

--- shiny/ui.R
+++ shiny/ui.R
@@ -3,6 +3,8 @@ shinyUI(
         # Application title
         headerPanel("Inference simulation"),
         sidebarPanel(
+            helpText("The core of the art of statistical inference is estimating the probability distribution of a given population by sampling. As a rule of thumb: the larger the sample, the closer will be its mean and standard deviation to that of the population."),
+            helpText("In this app we can play with the population and sample parameters and compare the inferred and the original probability distributions."),
             numericInput('mean', 'Mean of the population', 0, min = -5, max = 5, step = 0.1),
             numericInput('sd', 'Standard deviation of the population', 1, min = 0.1, max = 5, step = 0.1),
             h4('Larger samples must led to better inference. Give it a try.'),
@@ -16,7 +18,7 @@ shinyUI(
             h4('Inferred standard deviation'),
             verbatimTextOutput("sampleSd"),
             plotOutput('plot'),
-            h5('By Pablo Rodríguez. July 2015')
+            h5('By Pablo Rodriguez. July 2015')
         )
     )
 )
